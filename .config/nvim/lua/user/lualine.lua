e55b4669618a5dbcea2bfa35ac66e75f8d64e4e9
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-04-09 13:05:40 +0200

    Add initial config

---
+++ .config/nvim/lua/user/lualine.lua
@@ -0,0 +1,15 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+	return
+end
+
+lualine.setup({
+	options = {
+		theme = "gruvbox",
+	},
+	extensions = {
+		"fugitive",
+		"nvim-tree",
+		"toggleterm",
+	},
+})
