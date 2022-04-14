f2a64f3f5fea744843c8cc31c1343f04ce78aa56
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-04-14 17:54:53 +0200

    Fix ssh-agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,11 @@
 set -Ux EDITOR nvim
 set -g fish_greeting
+
+if test -z (pgrep ssh-agent | string collect)
+  eval (ssh-agent -c)
+  set -Ux SSH_ASKPASS "/usr/bin/ksshaskpass"
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
   starship init fish | source
