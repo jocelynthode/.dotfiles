d2054c237d9b31f4842cd48a0051598663ed8313
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-04-15 10:52:11 +0200

    Update dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,7 +8,7 @@ end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-  starship init fish | source
+  #  starship init fish | source
   kubectl completion fish | source
   fish_vi_key_bindings
 
