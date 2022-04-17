6f51ffaf462cc2cba3a96224bb2c4f6ed0912aa7
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-04-17 14:24:07 +0200

    Update nvim

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -119,9 +119,6 @@ cmp.setup({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
-	documentation = {
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-	},
 	experimental = {
 		ghost_text = true,
 		native_menu = false,
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -45,13 +45,23 @@ return packer.startup(function(use)
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("numToStr/Comment.nvim") -- Easily comment stuff
+--	use("numToStr/Comment.nvim") -- Easily comment stuff
+  use {
+    'numToStr/Comment.nvim',
+    tag = 'v0.6',
+  }
 	use("kyazdani42/nvim-web-devicons")
 	use("kyazdani42/nvim-tree.lua")
-	use("akinsho/bufferline.nvim")
+	use{
+    "akinsho/bufferline.nvim",
+    branch = "main",
+  }
 	use("moll/vim-bbye")
 	use("nvim-lualine/lualine.nvim")
-	use("akinsho/toggleterm.nvim")
+	use{
+    "akinsho/toggleterm.nvim",
+    branch = "main",
+  }
 	--[[ use "ahmedkhalf/project.nvim" ]]
 	use("lewis6991/impatient.nvim")
 	use("lukas-reineke/indent-blankline.nvim")
