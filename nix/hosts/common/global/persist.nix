ff0bd3020bb3b35344bdc7308833bae704d9e799
Jocelyn Thode <jocelyn.thode@gmail.com>
2022-07-04 18:34:25 +0200

    Refactor persist

--- nix/hosts/common/global/persist.nix
+++ nix/hosts/common/global/persist.nix
@@ -17,39 +17,49 @@
     ];
     users.jocelyn = {
       directories = [
+        # Base directories
         "Documents"
         "Downloads"
         "Music"
         "Pictures"
         "Videos"
         "Projects"
-        "Liip"
-        ".local/share/networkmanagement/certificates"
+        "Programs"
         "go"
-        ".config/fish"
-        ".local/share/fish"
 
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
-        ".local/share/Steam"
-        ".steam"
-        { directory = ".config/Signal"; mode = "0700"; }
-        { directory = ".config/Bitwarden"; mode = "0700"; }
-        { directory = ".kube"; mode = "0700"; }
+        # Work
+        "Liip"
+        ".local/share/networkmanagement/certificates" # Store VPN certificates
         ".config/taxi"
         ".local/share/taxi"
         ".local/share/zebra"
-        ".mozilla/firefox"
-        ".config/discord"
-        ".local/share/Mumble"
+        { directory = ".kube"; mode = "0700"; }
+
+        # Config
+        { directory = ".config/Signal"; mode = "0700"; }
+        { directory = ".config/Bitwarden"; mode = "0700"; }
+        ".config/fish"
         ".config/Slack"
         ".config/spotify"
         ".config/kdeconnect"
         ".config/pavucontrol-qt"
-        ".cache/betterlockscreen"
-        { directory = ".cache/rbw"; mode = "0700"; }
-        ".local/state/wireplumber" # Store default sinks/sources
+        ".config/discord"
 
+        # Local
+        { directory = ".local/share/keyrings"; mode = "0700"; }
+        ".local/share/fish"
+        ".local/share/Mumble"
+        ".local/state/wireplumber" # Store default sinks/sources
+        ".local/share/Steam"
+
+        # Cache
+        { directory = ".cache/rbw"; mode = "0700"; } # Store database cache
+        ".cache/betterlockscreen" # Store generated lockscreen images
+
+        # Misc
+        { directory = ".ssh"; mode = "0700"; }
+        ".mozilla/firefox"
+        ".steam"
         # TODO remove
         ".dotfiles"
       ];
